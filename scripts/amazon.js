5a5afa3a87691ac36c8496e32791f3e9bb903e5b
Rakib Hassan Nayem <rakibhassannayem@gmail.com>
2024-08-21 21:33:46 +0600

    13k

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -93,7 +93,7 @@ document.querySelectorAll('.js-add-to-cart')
         .innerHTML = cartQuantity;
 
       const addedMessage = document.querySelector(
-        `.js-added-to-cart ${productId}`
+        `.js-added-to-cart-${productId}`
       );
 
       addedMessage.classList.add('added-to-cart-visible');
