dbbb7dd0870b9f31da80183215c70d9c40cedbae
Rakib Hassan Nayem <rakibhassannayem@gmail.com>
2024-09-12 20:35:15 +0600

    14m

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -39,7 +39,7 @@ cart.forEach((cartItem) => {
                 </div>
                 <div class="product-quantity">
                   <span>
-                    Quantity: <span class="quantity-label">${cartItem.quantity}</span>
+                    Quantity: <span class="quantity-label js-quantity-label${matchingProduct.id}">${cartItem.quantity}</span>
                   </span>
                   <span class="update-quantity-link link-primary js-update-link"
                   data-product-id="${matchingProduct.id}">
@@ -159,5 +159,12 @@ document.querySelectorAll('.js-save-link')
       );
       const newQuantity = Number(quantityInput.value);
       updateQuantity(productId, newQuantity);
+
+      const quantityLabel = document.querySelector(
+        `.js-quantity-label-${productId}`
+      );
+      quantityLabel.innerHTML = newQuantity;
+
+      updateCartQuantity();
     });
   });
